d2499a4d74c3a9c11a2011254aa676d0ce0a77f9
Dan Appel <Dan.appel00@gmail.com>
2015-12-08 18:57:52 -0800

    Fill libvenice.rb

--- libvenice.rb
+++ libvenice.rb
@@ -1,9 +1,9 @@
 class Libvenice < Formula
-  desc ""
-  homepage ""
-  url ""
-  version ""
-  sha256 ""
+  desc "libvenice is a CSP framework for Swift."
+  homepage "https://github.com/Zewo"
+  url "https://github.com/Zewo/libvenice/archive/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "f07929679c08dcc5a8c196b0d09c6b245929d926bea6136026e2ce4c751e64d1"
 
   def install
     system "make"
