5ae490274e922d1ae6044420e5baac435373cee2
Dan Appel <Dan.appel00@gmail.com>
2015-12-08 19:53:20 -0800

    Fix SHA256 in http_parser formula

--- http_parser.rb
+++ http_parser.rb
@@ -3,7 +3,7 @@ class HttpParser < Formula
   homepage "https://github.com/Zewo/http_parser"
   url "https://github.com/Zewo/http_parser/archive/0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "220323cee627ad0330aee51f03d3c33e2f99cb98da9d50472557f481aebf259a"
+  sha256 "933456143f2773bda13c740672b4ca390e0a35f4aa9adf8c910b7fc717dbdab4"
 
   def install
     system "make"
