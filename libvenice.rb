7ff2ba6e18227d8c641408fb29d195b4fc6acacd
Anton <antonmes@users.noreply.github.com>
2016-02-16 12:05:52 +0300

    Update libvenice.rb

--- libvenice.rb
+++ libvenice.rb
@@ -1,9 +1,10 @@
 class Libvenice < Formula
   desc "libvenice is a CSP framework."
   homepage "https://github.com/Zewo/libvenice"
-  url "https://github.com/Zewo/libvenice/archive/0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "5fe69b675cda361b36738894fcf119b0507151445096b7eacb806068b278e798"
+  url "https://github.com/Zewo/libvenice/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "c11ba46fa14a3f7becb7823f23bc3fed67851207c7d445d918ed3a8c1063d66c"
+  head "https://github.com/antonmes/libvenice.git", :using => :git
 
   def install
     system "make"
