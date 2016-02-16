8c1825f2dfb6293dc380ebb481bd794c107ee3f0
Anton <antonmes@users.noreply.github.com>
2016-02-16 12:08:17 +0300

    fix head

--- libvenice.rb
+++ libvenice.rb
@@ -4,7 +4,7 @@ class Libvenice < Formula
   url "https://github.com/Zewo/libvenice/archive/0.2.0.tar.gz"
   version "0.2.0"
   sha256 "c11ba46fa14a3f7becb7823f23bc3fed67851207c7d445d918ed3a8c1063d66c"
-  head "https://github.com/antonmes/libvenice.git", :using => :git
+  head "https://github.com/Zewo/libvenice.git", :using => :git
 
   def install
     system "make"
