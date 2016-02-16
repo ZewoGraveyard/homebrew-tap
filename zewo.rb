c998fe25dbb9a8798b1cddb34e6084afa69774c5
Anton <antonmes@users.noreply.github.com>
2016-02-17 00:53:27 +0300

    using empty zip workaround

--- zewo.rb
+++ zewo.rb
@@ -2,14 +2,15 @@ class Zewo < Formula
   desc "Zewo is a set of open source libraries that help you build modern and blazing fast server software."
   homepage "https://github.com/Zewo/Zewo"
   version "0.2.0"
-  url "https://github.com/antonmes/brew-zewo/archive/v0.2.0.tar.gz"
-  sha256 "60c4866f6c5abcde640cc70b99370bc2a56a3173e88b1e750256ee836e12b4c7"
+  url "https://github.com/Zewo/ZewoCLI/archive/0.2.0.tar.gz"
+  sha256 "b2a1ebed73f58010408d8e42209f147ecf10d75d7d34f63752061ac5d6c350d9"
   
   depends_on "zewo/tap/libvenice"
   depends_on "zewo/tap/http_parser"
   depends_on "zewo/tap/uri_parser"
 
   def install
+    system "touch zewo"
     bin.install "zewo"
   end
 end
