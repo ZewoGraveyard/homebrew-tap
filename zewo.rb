8ff38476acdd478e15c47abb5a5361b8c285aacb
Anton <antonmes@users.noreply.github.com>
2016-02-17 01:36:26 +0300

    fix homepage

--- zewo.rb
+++ zewo.rb
@@ -1,6 +1,6 @@
 class Zewo < Formula
   desc "Zewo is a set of open source libraries that help you build modern and blazing fast server software."
-  homepage "https://github.com/Zewo/Zewo"
+  homepage "http://zewo.io"
   version "0.2.0"
   url "https://github.com/Zewo/ZewoCLI/archive/0.2.0.tar.gz"
   sha256 "b2a1ebed73f58010408d8e42209f147ecf10d75d7d34f63752061ac5d6c350d9"
