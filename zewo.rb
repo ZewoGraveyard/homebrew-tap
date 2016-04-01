44782ac407b9e860f2f91042c1614df326b417f9
Anton <antonmes@users.noreply.github.com>
2016-04-01 22:23:49 +0400

    Update zewo.rb

--- zewo.rb
+++ zewo.rb
@@ -1,7 +1,7 @@
 class Zewo < Formula
   desc "Zewo is a set of open source libraries that help you build modern and blazing fast server software."
   homepage "http://zewo.io"
-  version "0.2.0"
+  version "0.2.1"
   url "https://github.com/Zewo/homebrew-tap/releases/download/empty/empty.zip"
   sha256 "2317eed872d69afc610daa03a0a725fefd9c8853ca5415b2690b3338a49868a7"
   
